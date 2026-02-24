827b90897888d6847bcb3fb102b6ce59896a5e4c
Liza <ye.haponenko@ukma.edu.ua>
2026-02-24 10:28:47 +0200

    @media for header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,29 @@
+import { Link, useLocation } from "react-router-dom";
+
 function Header() {
+    const location = useLocation();
+
+    const hideAuthButtons =
+        location.pathname === "/signin" ||
+        location.pathname === "/signup";
+
     return (
         <header className="header">
             <div id="logo">Éternité</div>
 
-            <div className="account-buttons">
-                <button className="sign-in-button" id="button">SIGN IN</button>
-                <button className="sign-up-button" id="button">SIGN UP</button>
-            </div>
+            {!hideAuthButtons && (
+                <div className="account-buttons">
+                    <Link to="/signin">
+                        <button className="sign-in-button" id="button">SIGN IN</button>
+                    </Link>
+
+                    <Link to="/signup">
+                        <button className="sign-up-button" id="button">SIGN UP</button>
+                    </Link>
+                </div>
+            )}
         </header>
-    )
+    );
 }
 
 export default Header;
