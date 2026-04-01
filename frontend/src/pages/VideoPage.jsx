bf4c3e1377fe24200702da9191faaf2e66059eac
Liza <ye.haponenko@ukma.edu.ua>
2026-04-01 13:37:36 +0300

    main page text

--- frontend/src/pages/VideoPage.jsx
+++ frontend/src/pages/VideoPage.jsx
@@ -9,8 +9,26 @@ function VideoPage() {
                 playsInline
             >
                 <source src="/videos/mainbackgroundvideo.mp4" type="video/mp4" />
-                Your browser does not support the video tag.
             </video>
+            <section className="main-page-section">
+                <div className="side-label side-label--left">
+                    <span>/ ÉTERNITÉ /</span>
+                </div>
+                <div className="main-page-text">
+                    <p className="main-page-subtitle" id="nav">/ FOR THE CREATIVITY /</p>
+
+                    <p className="main-page-title">
+                        EXPLORE THE WORLD<br />
+                        <em className="main-page-title--italic">full of </em>
+                        BEAUTY AND INSPIRATION
+                    </p>
+
+                    <p className="main-page-bottom" id="nav">//</p>
+                </div>
+                <div className="side-label side-label--right">
+                    <span>/ ÉTERNITÉ /</span>
+                </div>
+            </section>
         </div>
     );
 }
